40815005ee1006d9aa2068509e51aab92bb0ab4f
Innocent_Bern <innocentbern22@gmail.com>
2023-07-24 23:08:09 +0300

    Task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,14 +2,15 @@
 
 /**
  * partition - Divides array into 2 partitions
- * @low: integer input
- * @high: integer input
+ * @arr: pointer to array of integers
+ * @low: starting index
+ * @high: last index
  * @size: number of items in the array
- * Return: index of the 
+ * Return: int
 */
 int partition(int *arr, int low, int high, size_t size)
 {
-	int pivot = arr[high], i = low - 1, tmp,j;
+	int pivot = arr[high], i = low - 1, tmp, j;
 
 	for (j = low; j <= high - 1; j++)
 	{
@@ -19,7 +20,6 @@ int partition(int *arr, int low, int high, size_t size)
 			tmp = arr[i];
 			arr[i] = arr[j];
 			arr[j] = tmp;
-			print_array(arr, size);
 		}
 	}
 	tmp = arr[i + 1];
@@ -29,19 +29,30 @@ int partition(int *arr, int low, int high, size_t size)
 	return (i + 1);
 }
 /**
+ * part_sort - recursive function sorts array of integers
+ * @array: pointer to array of integers
+ * @size: number of items in array
+ * @low: starting index
+ * @high: lasing index
+*/
+void part_sort(int *array, size_t size, int low, int high)
+{
+	int pi;
+
+	if (low < high)
+	{
+		pi = partition(array, low, high, size);
+
+		part_sort(array, size, low, pi - 1);
+		part_sort(array, size, pi + 1, high);
+	}
+}
+/**
  * quick_sort - function that sorts an array of integers in ascending order
  * @array: pointer to array of integers
  * @size: number of items in the array.
 */
 void quick_sort(int *array, size_t size)
 {
-	int pix;
-
-	if (size > 0)
-	{
-		pix = partition(array, 0, size, size);
-
-		quick_sort(array, size - pix);
-		quick_sort(array, size);
-	}
+	part_sort(array, size, 0, size - 1);
 }
